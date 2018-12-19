d1d76f42f74b9e12b7953096116c8795bd1c20cb
Andrew St. Pierre <astpier@purdue.edu>
2018-12-19 18:01:56 -0500

    testing round 1

--- dev_config.py
+++ dev_config.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 ###############################################################################
 # Written by:   Andrew St. Pierre
@@ -12,12 +12,13 @@ import getopt
 import shutil
 import logging
 import getpass
-import ConfigParser
+import yaml
+
 
 # Global variables
 #-----------------------------------------------------------------------------
-_VERSION = "1.0.0"
-_CONF_FILE = "dev_config.cfg"
+VERSION = "1.0.0"
+OPTIONS_FILE = "configs.yml"
 
 # System commands
 #-----------------------------------------------------------------------------
@@ -51,27 +52,26 @@ def init():
     Init the script
     """
     # Globals variables
-    global _VERSION
-    global _DEBUG
+    global VERSION
 
 def syntax():
     """
     Print the script syntax
     """
-    print "Ubuntu 18.10 development configuration script version %s\n" % _VERSION
-    print "Usage: sudo dev_config.py [-h] [-v]"
-    print "  -h         : Print the syntax and exit"
-    print "  -v         : Print the version and exit\n"
-    print "Examples:\n"
-    print " # sudo dev_config.py"
-    print " > Run the script with the default configuration file"
-    print "   %s\n\n" % _CONF_FILE
+    print("Ubuntu 18.10 development configuration script version %s\n" % VERSION)
+    print("Usage: sudo dev_config.py [-h] [-v]")
+    print("  -h         : Print the syntax and exit")
+    print("  -v         : Print the version and exit\n")
+    print("Examples:\n")
+    print(" # sudo dev_config.py")
+    print(" > Run the script with the default configuration file")
+    print("   %s\n\n" % OPTIONS_FILE)
 
 def version():
     """
     Print the script version
     """
-    sys.stdout.write("Script version %s" % _VERSION)
+    sys.stdout.write("Script version %s" % VERSION)
     sys.stdout.write(" (running on %s %s)\n" % (platform.system(), platform.machine()))
 
 def isroot():
@@ -79,11 +79,11 @@ def isroot():
     Check if the user is root
     Return TRUE if user is root
     """
-    return (os.geteuid() == 0)
+    return(os.geteuid() == 0)
 
 def showexec(description, command, exitonerror = 0, presskey = 0, waitmessage = ""):
     """
-    Exec a system command with a pretty status display (Running / Ok / Warning / Error)
+    Exec a system cmd w a pretty status display (Running / Ok / Warning / Error)
     By default (exitcode=0), the function did not exit if the command failed
     """
 
@@ -102,8 +102,8 @@ def showexec(description, command, exitonerror = 0, presskey = 0, waitmessage = 
         status = "[ ENTER ]"
     else:
         status = "[Running]"
-    statuscolor = colors.BLUE
-    sys.stdout.write (colors.NO + "%s" % waitmessage + statuscolor + "%s" % status.rjust(79-len(waitmessage)) + colors.NO)
+    statuscolor = colors.ORANGE
+    sys.stdout.write(colors.NO + "%s" % waitmessage + statuscolor + "%s" % status.rjust(79-len(waitmessage)) + colors.NO)
     sys.stdout.flush()
 
     # Wait keypressed (optionnal)
@@ -172,19 +172,19 @@ def waitenterpressed(message = "Press ENTER to continue..."):
     raw_input(message)
     return 0
 
-
 def main(argv):
     """
     Main function
     """
+    # Parse the input arguments
     try:
         opts, args = getopt.getopt(argv, "c:hv", ["config", "help", "version"])
     except getopt.GetoptError:
         syntax()
         exit(2)
 
-    config_file = ""
-    config_url = _CONF_FILE
+    # The default configurations file
+    config_file = OPTIONS_FILE
 
     # Check if user wants to use help or see version
     for opt, arg in opts:
@@ -195,101 +195,67 @@ def main(argv):
             version()
             exit()
 
-    # Are your root ?
+    # Are you root?
     if(not isroot()):
         showexec ("Script should be run as root", " ", exitonerror = 1)
 
-    # Check the Ubuntu version
-    _UBUNTU_VERSION = os.uname()
+    # Check the Ubuntu version (TODO: get more user info)
+    UBUNTU_VERSION = os.uname()
 
     # Read the configuration file
-    if (config_file == ""):
-        config_file = "dev_config.cfg"
-        #showexec ("Download the configuration file", "rm -f "+config_file+" ; "+_WGET+" -O "+config_file+" "+config_url)
-    #config = ConfigParser.RawConfigParser()
-    #config.read(config_file)
+    with open(config_file, 'r') as fyaml:
+        configs = yaml.load(fyaml)
 
-    '''
-    if (config.has_section("gnome3") and config.has_section("unity")):
-        showexec ("Can not use both Gnome 3 and Unity, please change your .cfg file", "gnome3etunitygrosboulet", exitonerror = 1)
-    '''
+    # Install all requested packages
+    if "packages" in configs:
+        # Parse and exec pre-actions
+        for pckg in configs["packages"]:
+            # TESTING WITH virtualenv and htop
+            command_uninstall = _APT_REMOVE+" "+pckg
+            command_install = _APT_INSTALL+" "+pckg
+            showexec("Uninstall package "+pckg, command_uninstall)
+            showexec("Install package "+pckg, command_install)
+        # Update repos
+        showexec ("Update repositories", _APT_UPDATE)
+        # Upgrade system
+        showexec ("System upgrade (takes a while, be patient...)", _APT_UPGRADE)
 
-    # Parse and exec pre-actions
-    '''
-    for action_name, action_cmd in config.items("preactions"):
-        showexec ("Execute preaction "+action_name.lstrip("action_"), action_cmd)
-    '''
+    # Download the dotfiles from specified locations and use them
+    if "dotfiles" in configs:
+        for df in configs["dotfiles"]:
+            if df == "bashrc":
+                url = configs["dotfiles"]["bashrc"]
+                showexec("Downloading .bashrc from github...", _WGET+" -O $HOME/.bashrc.bak "+url)
+                showexec("Moving .bashrc into proper file...", "mv $HOME/.bashrc.bak $HOME/.bashrc")
 
-    # Parse and install repositories
-    '''
-    pkg_list_others = {}
-    for item_type, item_value in config.items("repos"):
-        if (item_type.startswith("ppa_")):
-            showexec ("Install repository "+item_type.lstrip("ppa_"), _APT_ADD+" "+item_value)
-        elif (item_type.startswith("url_")):
-            showexec ("Install repository "+item_type.lstrip("url_"), _APT_ADD+" \\\"deb "+item_value+"\\\"")
-        elif (item_type.startswith("key_")):
-            showexec ("Install key for the "+item_type.lstrip("key_")+" repository", _APT_KEY+" "+item_value)
-        elif (item_type.startswith("pkg_")):
-            pkg_list_others[item_type] = item_value
-    '''
-    # Update repos
-    #showexec ("Update repositories", _APT_UPDATE)
+            elif df == "bash_aliases":
+                url = configs["dotfiles"]["bash_aliases"]
+                showexec("Downloading .bash_aliases from github...", _WGET+" -O $HOME/.bash_aliases.bak "+url)
+                showexec("Moving .bash_aliases into proper file...", "mv $HOME/.bash_aliases.bak $HOME/.bash_aliases")
+            else: print("Unrecognized dotfile category: %s"%df)
+        showexec("Running your new .bashrc script!", "source $HOME/.bashrc")
 
-    # Upgrade system
-    #showexec ("System upgrade (~20 mins, please be patient...)", _APT_UPGRADE)
-
-    # Parse and install packages
-    '''
-    for pkg_type, pkg_list in config.items("packages"):
-        if (pkg_type.startswith("remove_")):
-            showexec ("Remove packages "+pkg_type.lstrip("remove_"), _APT_REMOVE+" "+pkg_list)
-        else:
-            showexec ("Install packages "+pkg_type, _APT_INSTALL+" "+pkg_list)
-
-    # Install packages related to repositories
-    #~ print pkg_list_others
-    for pkg in pkg_list_others.keys():
-        showexec ("Install packages "+pkg, _APT_INSTALL+" "+pkg_list_others[pkg])
-
-    # Download and install dotfiles: vimrc, prompt...
-    if (config.has_section("dotfiles")):
-        # Create the bashrc.d subfolder
-        showexec ("Create the ~/.bashrc.d subfolder", "mkdir -p $HOME/.bashrc.d")
-        if (config.has_option("dotfiles", "bashrc")):
-            showexec ("Download bash main configuration file", _WGET+" -O $HOME/.bashrc "+config.get("dotfiles", "bashrc"))
-        if (config.has_option("dotfiles", "bashrc_prompt")):
-            showexec ("Download bash prompt configuration file", _WGET+" -O $HOME/.bashrc.d/bashrc_prompt "+config.get("dotfiles", "bashrc_prompt"))
-        if (config.has_option("dotfiles", "bashrc_aliases")):
-            showexec ("Download bash aliases configuration file", _WGET+" -O $HOME/.bashrc.d/bashrc_aliases "+config.get("dotfiles", "bashrc_aliases"))
-        showexec ("Install the bash configuration file", "chown -R $USERNAME:$USERNAME $HOME/.bashrc*")
-        # Vim
-        if (config.has_option("dotfiles", "vimrc")):
-            showexec ("Donwload the Vim configuration file", _WGET+" -O $HOME/.vimrc "+config.get("dotfiles", "vimrc"))
-            showexec ("Install the Vim configuration file", "chown -R $USERNAME:$USERNAME $HOME/.vimrc")
-
-        # Htop
-        if (config.has_option("dotfiles", "htoprc")):
-            showexec ("Download the Htop configuration file", _WGET+" -O $HOME/.htoprc "+config.get("dotfiles", "htoprc"))
-            showexec ("Install the Htop configuration file", "chown -R $USERNAME:$USERNAME $HOME/.htoprc")
-
-        # Pythonrc
-        if (config.has_option("dotfiles", "pythonrc")):
-            showexec ("Download the Pythonrc configuration file", _WGET+" -O $HOME/.pythonrc "+config.get("dotfiles", "pythonrc"))
-            showexec ("Install the Pythonrc configuration file", "chown -R $USERNAME:$USERNAME $HOME/.pythonrc")
-    '''
+    # Clone the vimrc settings into home dir and add mouse clicking too
+    if "vimrc" in configs:
+        instr = configs["vimrc"]["clone"]
+        showexec("Cloning the vim settings repo from github...", instr+" $HOME")
+        with open("$HOME/.vimrc", "a") as vmrc:
+            vmrc.write(configs["vimrc"]["addMouse"])
+        instr = configs["vimrc"]["runit"]
+        showexec("Implementing your new vim preferences...", instr)
 
     # End of the script
-    print("---")
-    print("End of the script.")
-    print(" - Configuration file: "+config_file)
-    print("")
-    print("Please restart your session to complete.")
-    print("---")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("End of the configuration script.")
+    print(" => configuration file: "+config_file+"\n")
+    print("Restart your session to complete.")
+    if(getstring("Would you like to reboot now? (y/n)") == "y"):
+        os.system("shutdown /r /t 1")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
 
 # Main program
 #-----------------------------------------------------------------------------
-
 if __name__ == "__main__":
     init()
     main(sys.argv[1:])
