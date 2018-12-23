cc37d4834ac07d4306fc470c2e4ae3eb528b0660
Andrew St. Pierre <astpier@purdue.edu>
2018-12-23 15:42:26 -0500

    adding color

--- dev_config.py
+++ dev_config.py
@@ -13,11 +13,12 @@ import shutil
 import logging
 import getpass
 import yaml
-
+import colorama
+from colorama import Fore, Back, Style
 
 # Global variables
 #-----------------------------------------------------------------------------
-VERSION = "1.1.0"
+VERSION = "1.2.1"
 OPTIONS_FILE = "configs.yml"
 
 # System commands
@@ -29,21 +30,33 @@ _APT_UPDATE = "DEBIAN_FRONTEND=noninteractive apt-get -y update"
 _APT_UPGRADE = "DEBIAN_FRONTEND=noninteractive apt-get -y upgrade"
 _WGET = "wget"
 
-# Classes
-#-----------------------------------------------------------------------------
-class colors:
-    RED = '\033[91m'
-    GREEN = '\033[92m'
-    BLUE = '\033[94m'
-    ORANGE = '\033[93m'
-    NO = '\033[0m'
+FRED = Fore.RED
+FGREEN = Fore.GREEN
+FBLUE = Fore.BLUE
+FWHITE = Fore.WHITE
+FCYAN = Fore.CYAN
+FMAGENTA = Fore.MAGENTA
+FYELLOW = Fore.YELLOW
+FBLACK = Fore.BLACK
+BRED = Back.RED
+BGREEN = Back.GREEN
+BLUE = Back.BLUE
+BWHITE = Back.WHITE
+BCYAN = Back.CYAN
+BMAGENTA = Back.MAGENTA
+BYELLOW = Back.YELLOW
+SDIM = Style.DIM
+SNORMAL = Style.NORMAL
+SBRIGHT = Style.BRIGHT
+BOLD = '\x1b[1;37m'
+QUESTION = SBRIGHT+FBLUE+Back.RESET
+OLDQ = SBRIGHT+FMAGENTA+Back.RESET
 
-    def disable(self):
-        self.RED = ''
-        self.GREEN = ''
-        self.BLUE = ''
-        self.ORANGE = ''
-        self.NO = ''
+WARNING = SDIM+FYELLOW+Back.RESET
+CHOICES = SNORMAL+FCYAN+Back.RESET
+ERROR = SBRIGHT+FRED+BYELLOW
+SUCCESS = SBRIGHT+FGREEN+Back.RESET
+RESET = Style.RESET_ALL+Fore.RESET+Back.RESET
 
 # Functions
 #-----------------------------------------------------------------------------
@@ -101,9 +114,9 @@ def showexec(description, command, exitonerror = 0, presskey = 0, waitmessage = 
     if (presskey == 1):
         status = "[ ENTER ]"
     else:
-        status = "[Running]"
-    statuscolor = colors.ORANGE
-    sys.stdout.write(colors.NO + "%s" % waitmessage + statuscolor + "%s" % status.rjust(79-len(waitmessage)) + colors.NO)
+        status = "[ IN PROGRRESS ]"
+    statuscolor = FYELLOW
+    sys.stdout.write(RESET+SDIM+"%s"%waitmessage+statuscolor+SBRIGHT+"%s"%status.rjust(80-len(waitmessage))+RESET)
     sys.stdout.flush()
 
     # Wait keypressed (optionnal)
@@ -119,17 +132,17 @@ def showexec(description, command, exitonerror = 0, presskey = 0, waitmessage = 
 
     # Display the result
     if ((returncode == 0) or (returncode == 25600)):
-        status = "[  OK   ]"
-        statuscolor = colors.GREEN
+        status = "[ DONE ]"
+        statuscolor = SUCCESS
     else:
         if exitonerror == 0:
-            status = "[Warning]"
-            statuscolor = colors.ORANGE
+            status = "[ WARNING ]"
+            statuscolor = WARNING
         else:
-            status = "[ Error ]"
-            statuscolor = colors.RED
+            status = "[ ERROR ]"
+            statuscolor = ERROR
 
-    sys.stdout.write(colors.NO + "\r%s" % description + statuscolor + "%s\n" % status.rjust(79-len(description)) + colors.NO)
+    sys.stdout.write(RESET+SDIM+"\r%s"%description+statuscolor+SBRIGHT+"%s\n"%status.rjust(80-len(description))+RESET)
 
     # Stop the program if returncode and exitonerror != 0
     if ((returncode != 0) & (exitonerror != 0)):
@@ -161,6 +174,12 @@ def getstring(message = "Enter a value: "):
         pass
     return inpt
 
+def getChar():
+    userInput = ''
+    while len(userInput) != 1:
+        userInput = input("=> ")
+    return userInput.lower()
+
 def waitenterpressed(message = "Press ENTER to continue..."):
     """
     Wait until ENTER is pressed
@@ -171,6 +190,40 @@ def waitenterpressed(message = "Press ENTER to continue..."):
         pass
     raw_input(message)
     return 0
+
+def doPackages(configs):
+    # Install all requested packages
+    if "packages" in configs:
+        # Parse and exec pre-actions
+        for pckg in configs["packages"]:
+            # TESTING WITH virtualenv and htop
+            command_install = _APT_INSTALL+" "+pckg
+            showexec("Install package "+pckg, command_install)
+        # Update repos
+        showexec ("Update repositories", _APT_UPDATE)
+        # Upgrade system
+        showexec ("System upgrade (takes a while, be patient...)", _APT_UPGRADE)
+
+def doDotfiles(configs):
+    # Download the dotfiles from specified locations and use them
+    for df in configs["dotfiles"]:
+        if df == "bashrc":
+            url = configs["dotfiles"]["bashrc"]
+            showexec("Downloading .bashrc from github...", _WGET+" -O $HOME/.bashrc.bak "+url)
+            showexec("Moving .bashrc into proper file...", "mv $HOME/.bashrc.bak $HOME/.bashrc")
+
+        elif df == "bash_aliases":
+            url = configs["dotfiles"]["bash_aliases"]
+            showexec("Downloading .bash_aliases from github...", _WGET+" -O $HOME/.bash_aliases.bak "+url)
+            showexec("Moving .bash_aliases into proper file...", "mv $HOME/.bash_aliases.bak $HOME/.bash_aliases")
+        else: print("Unrecognized dotfile category: %s"%df)
+    showexec("Running your new .bashrc script!", "source $HOME/.bashrc")
+
+def doVimstuff(configs):
+    # Clone the vimrc settings into home dir
+    instr = configs["vimrc"]["clone"]
+    showexec("Cloning the vim settings repo from github...", instr+" $HOME")
+    instr = configs["vimrc"]["runit"]
 
 def main(argv):
     """
@@ -185,6 +238,7 @@ def main(argv):
 
     # The default configurations file
     config_file = OPTIONS_FILE
+    colorama.init()
 
     # Check if user wants to use help or see version
     for opt, arg in opts:
@@ -195,66 +249,90 @@ def main(argv):
             version()
             exit()
 
+    note="Welcome to dev-config! v."
+    sys.stdout.write(BWHITE+SBRIGHT+FBLACK+note+VERSION+" "*(80-len(note)-len(VERSION))+"\n")
+
     # Are you root?
+    leftmessage = "Does user have root privs?"
     if(not isroot()):
         showexec ("Script should be run as root", " ", exitonerror = 1)
+    rightmessage = "[ PASSED ]"
+    sys.stdout.write(OLDQ+"%s"%leftmessage+SUCCESS+"%s"%rightmessage.rjust(80-len(leftmessage))+RESET)
 
     # Check the Ubuntu version (TODO: get more user info)
     UBUNTU_VERSION = os.uname()
 
-    # Read the configuration file
+    # Read the configuration file and lets see what we will do with it
     with open(config_file, 'r') as fyaml:
         configs = yaml.load(fyaml)
+    note="Now customizing your experience w. your configurations from your YAML file."
+    sys.stdout.write(BWHITE+SBRIGHT+FBLACK+note+" "*(80-len(note))+"\n")
 
-    # Install all requested packages
-    '''
-    if "packages" in configs:
-        # Parse and exec pre-actions
-        for pckg in configs["packages"]:
-            # TESTING WITH virtualenv and htop
-            command_install = _APT_INSTALL+" "+pckg
-            showexec("Install package "+pckg, command_install)
-        # Update repos
-        showexec ("Update repositories", _APT_UPDATE)
-        # Upgrade system
-        showexec ("System upgrade (takes a while, be patient...)", _APT_UPGRADE)
-    '''
-    # Download the dotfiles from specified locations and use them
-    '''
     if "dotfiles" in configs:
-        for df in configs["dotfiles"]:
-            if df == "bashrc":
-                url = configs["dotfiles"]["bashrc"]
-                showexec("Downloading .bashrc from github...", _WGET+" -O $HOME/.bashrc.bak "+url)
-                showexec("Moving .bashrc into proper file...", "mv $HOME/.bashrc.bak $HOME/.bashrc")
+        # Ask if user wants to do this
+        leftmessage = "Update dotfiles?"
+        rightmessage = "[ (y)es / (n)o ]"
+        sys.stdout.write(QUESTION+"%s"%leftmessage+RESET+CHOICES+"%s"%rightmessage.rjust(80-len(leftmessage))+RESET)
+        sys.stdout.flush()
+        if(getChar() == "y"):
+            doDotfiles(configs)
+            leftmessage = "Dotfiles successfully updated."
+            rightmessage = "[ DONE ]"
+            sys.stdout.write(OLDQ+"%s"%leftmessage+RESET+SUCCESS+"%s"%rightmessage.rjust(80-len(leftmessage))+RESET)
+        else:
+            leftmessage = "Dotfiles left unchanged."
+            rightmessage = "[ SKIPPED ]"
+            sys.stdout.write(OLDQ+"%s"%leftmessage+RESET+WARNING+"%s"%rightmessage.rjust(80-len(leftmessage))+RESET)
 
-            elif df == "bash_aliases":
-                url = configs["dotfiles"]["bash_aliases"]
-                showexec("Downloading .bash_aliases from github...", _WGET+" -O $HOME/.bash_aliases.bak "+url)
-                showexec("Moving .bash_aliases into proper file...", "mv $HOME/.bash_aliases.bak $HOME/.bash_aliases")
-            else: print("Unrecognized dotfile category: %s"%df)
-        showexec("Running your new .bashrc script!", "source $HOME/.bashrc")
-    '''
-    # Clone the vimrc settings into home dir and add mouse clicking too
-    '''
+    if "packages" in configs:
+        # Ask if user wants to do this
+        leftmessage = "Install packages on list in config.yml?"
+        rightmessage = "[ (y)es / (n)o ]"
+        sys.stdout.write(QUESTION+"%s"%leftmessage+RESET+CHOICES+"%s"%rightmessage.rjust(80-len(leftmessage))+RESET)
+        sys.stdout.flush()
+        if(getChar() == "y"):
+            doPackages(configs)
+            leftmessage = "Packages successfully installed."
+            rightmessage = "[ DONE ]"
+            sys.stdout.write(OLDQ+"%s"%leftmessage+RESET+SUCCESS+"%s"%rightmessage.rjust(80-len(leftmessage))+RESET)
+        else:
+            leftmessage = "Packages left unchanged."
+            rightmessage = "[ SKIPPED ]"
+            sys.stdout.write(OLDQ+"%s"%leftmessage+RESET+WARNING+"%s"%rightmessage.rjust(80-len(leftmessage))+RESET)
+
     if "vimrc" in configs:
-        instr = configs["vimrc"]["clone"]
-        showexec("Cloning the vim settings repo from github...", instr+" $HOME")
-        with open("$HOME/.vimrc", "a") as vmrc:
-            vmrc.write(configs["vimrc"]["addMouse"])
-        instr = configs["vimrc"]["runit"]
-        showexec("Implementing your new vim preferences...", instr)
-    '''
-    # End of the script
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    print("End of the configuration script.")
-    print(" => configuration file: "+config_file+"\n")
-    print("Restart your session to complete.")
-    if(getstring("Would you like to reboot now? (y/n)") == "y"):
-        print("REBOOT COMMENTED OUT...")
-        #os.system("reboot")
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        # Ask if user wants to do this
+        leftmessage = "Would you like to update your vim settings?"
+        rightmessage = "[ (y)es / (n)o ]"
+        sys.stdout.write(QUESTION+"%s"%leftmessage+RESET+CHOICES+"%s"%rightmessage.rjust(80-len(leftmessage))+RESET)
+        sys.stdout.flush()
+        if(getChar() == "y"):
+            #doVimstuff(configs)
+            leftmessage = "Vim configurations successfully changed."
+            rightmessage = "[ DONE ]"
+            sys.stdout.write(OLDQ+"%s"%leftmessage+RESET+SUCCESS+"%s"%rightmessage.rjust(80-len(leftmessage))+RESET)
+        else:
+            leftmessage = "Vim configurations left unchanged."
+            rightmessage = "[ SKIPPED ]"
+            sys.stdout.write(OLDQ+"%s"%leftmessage+RESET+WARNING+"%s"%rightmessage.rjust(80-len(leftmessage))+RESET)
 
+    # End of the script
+    note="End of configuration script. Used configuration file: "
+    sys.stdout.write(BWHITE+SBRIGHT+FBLACK+note+config_file+" "*(80-len(note)-len(config_file))+"\n")
+
+    leftmessage = "Would you like to reboot now?"
+    rightmessage = "[ (y)es / (n)o ]"
+    sys.stdout.write(QUESTION+"%s"%leftmessage+RESET+CHOICES+"%s"%rightmessage.rjust(80-len(leftmessage))+RESET)
+    if(getChar() == "y"):
+        leftmessage = "Rebooting now!"
+        rightmessage = "[ REBOOTING ]"
+        sys.stdout.write(OLDQ+"%s"%leftmessage+RESET+SUCCESS+"%s"%rightmessage.rjust(80-len(leftmessage))+RESET)
+        #os.system("reboot")
+    else:
+        leftmessage = "Ok, reboot later."
+        rightmessage = "[ REBOOT POSTPONED ]"
+        sys.stdout.write(OLDQ+"%s"%leftmessage+RESET+WARNING+"%s"%rightmessage.rjust(80-len(leftmessage))+RESET)
+    sys.stdout.write(RESET+BWHITE+" "*80+RESET)
 
 # Main program
 #-----------------------------------------------------------------------------
